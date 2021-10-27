1428ab8f254fa692697fdec82f9e1b45fbb732a2
qianxunj@163.com <Cy742698!@>
2021-10-27 22:38:09 +0800

    20210127

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import "./common/uni.css";
+	
+	/* 引入 图标库*/
+	@import "./common/iconfont.css";
+	
+	@import "./common/animate.css";
+</style>
